063c852a2b425039c5a9690d1f3e47883f8235b0
Juan Patarroyo Ardila <juanestebanpatarroyo@gmail.com>
2023-03-23 23:53:41 -0500

    Login component
    In this commit I add login component, still in progress to build it

--- control-client-frontEnd/src/app/components/login/login.component.ts
+++ control-client-frontEnd/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Report } from 'notiflix';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  email!: string;
+  password!: string;
+
+  constructor(private router: Router){
+    
+  }
+
+  login(){
+
+  }
 }
